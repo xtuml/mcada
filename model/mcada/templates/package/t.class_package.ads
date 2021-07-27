70cd6c9bce010fe352f2cc5ac0f20ecf675370ce
Cortland Starrett <cort@roxsoftware.com>
2021-07-27 12:16:47 -0400

    job:  #12149  Adding types subsystem in early form.

--- model/mcada/templates/package/t.class_package.ads
+++ model/mcada/templates/package/t.class_package.ads
@@ -3,6 +3,10 @@
 
 package ${root_package.name}.${domain_package.name}.${self.name} is
 
+--**************  Object ${self.long_name} Information **************
+
+Object_String : String := "${self.name}";
+
 ${record_body}
 
 end ${root_package.name}.${domain_package.name}.${self.name};
